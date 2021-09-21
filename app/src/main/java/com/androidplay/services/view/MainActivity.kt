50ba9e466408c89fe6efa49a0d8b0d0f821ad0de
ANKUBOSE <46471379+bosankus@users.noreply.github.com>
2021-09-21 16:19:35 +0530

    Screen UI changes + CircleCi change * Activity UI done * Added new color palette * Changed circlci config.yml file to support testing

--- app/src/main/java/com/androidplay/services/view/MainActivity.kt
+++ app/src/main/java/com/androidplay/services/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.androidplay.services
+package com.androidplay.services.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androidplay.services.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

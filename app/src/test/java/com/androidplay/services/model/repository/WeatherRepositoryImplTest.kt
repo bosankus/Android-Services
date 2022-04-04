106af560634ba8c68c0bc0ea587adaaa16e11876
bosankus <ankush@androidplay.in>
2022-04-04 17:48:10 +0530

    Testing- Added Repository unit test

---
+++ app/src/test/java/com/androidplay/services/model/repository/WeatherRepositoryImplTest.kt
@@ -0,0 +1,48 @@
+package com.androidplay.services.model.repository
+
+import com.androidplay.services.model.model.Weather
+import com.androidplay.services.model.network.WeatherApiInterface
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherRepositoryImplTest {
+
+    private val api: WeatherApiInterface = mock()
+
+    @Spy
+    private val repository: WeatherRepository = WeatherRepositoryImpl(api)
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun getWeather_calls_apiSuccessfully() {
+        runBlocking {
+            val areaName = "Kolkata"
+            repository.getWeather(areaName)
+            verify(api, times(1)).getWeather(areaName)
+        }
+    }
+
+    @Test
+    fun getWeather_onWrongLocationArgument_returnsNullWeatherId() {
+        runBlocking {
+            val areaName = "Ko"
+            val result: Weather? = repository.getWeather(areaName)
+            assertThat(result?.id).isNull()
+        }
+    }
+
+}

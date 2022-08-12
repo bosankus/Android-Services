66bd39226adc06bd508ec9892ff50baec0d3b7ec
bosankus <ankush@androidplay.in>
2022-08-12 12:32:57 +0530

    Fixed datastore dependencies and imports - added dependencies for datastore preference

--- app/src/main/java/com/androidplay/services/model/persistance/DataStoreManagerImpl.kt
+++ app/src/main/java/com/androidplay/services/model/persistance/DataStoreManagerImpl.kt
@@ -12,30 +12,31 @@ import com.androidplay.services.utils.Extensions.toCelsius
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-
 private val TEMP_PREF_KEY = stringPreferencesKey("TEMP_PREF")
 private val AREA_PREF_KEY = stringPreferencesKey("AREA_KEY")
 private val Context.weatherPreferenceDataStore: DataStore<Preferences> by preferencesDataStore(
     name = DATASTORE_PREF_NAME
 )
 
-class DataStoreManagerImpl(private val context: Context): DataStoreManager {
+class DataStoreManagerImpl(context: Context): DataStoreManager {
+
+    private val weatherPreferenceDataStore = context.weatherPreferenceDataStore
 
     override suspend fun saveData(weather: Weather) {
-        context.weatherPreferenceDataStore.edit { preferences ->
+        weatherPreferenceDataStore.edit { preferences ->
             preferences[TEMP_PREF_KEY] = weather.main.temp.toCelsius()
             preferences[AREA_PREF_KEY] = weather.name
         }
     }
 
     override suspend fun getAreaName(): Flow<String?> {
-        return context.weatherPreferenceDataStore.data.map { preferences ->
+        return weatherPreferenceDataStore.data.map { preferences ->
             preferences[AREA_PREF_KEY]
         }
     }
 
     override suspend fun getTemperature(): Flow<String?> {
-        return context.weatherPreferenceDataStore.data.map { preferences ->
+        return weatherPreferenceDataStore.data.map { preferences ->
             preferences[TEMP_PREF_KEY]
         }
     }

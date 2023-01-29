12ffcbbf3c0c18345db8403104fc99f0e33af9df
Ankush Bose <ankushbose5@gmail.com>
2023-01-29 19:10:06 +0530

    Removed unused commented code

--- app/src/main/java/com/androidplay/services/model/network/WeatherApiService.kt
+++ app/src/main/java/com/androidplay/services/model/network/WeatherApiService.kt
@@ -12,28 +12,9 @@ import retrofit2.http.Query
  */
 interface WeatherApiService {
 
-    /*implementation method to get current temperature of hardcoded city*/
     @GET("data/2.5/weather")
     suspend fun getWeather(
         @Query("q") location: String,
         @Query("APPID") AppId: String = BuildConfig.WEATHER_KEY
     ): Weather?
-
-    /*companion object {
-
-        private val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-
-        // TODO: Provide okHttpClient with network interceptors via DI
-        operator fun invoke(): WeatherApiInterface {
-            return Retrofit.Builder()
-                .baseUrl(WEATHER_URL)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .build()
-                .create(WeatherApiInterface::class.java)
-        }
-    }*/
 }

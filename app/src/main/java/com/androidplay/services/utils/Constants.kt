bf36065c28b8d61208aa358037dc7d03ea1edeb1
Ankush Bose <ankushbose5@gmail.com>
2023-01-29 08:21:43 +0530

    Updated main api base url

--- app/src/main/java/com/androidplay/services/utils/Constants.kt
+++ app/src/main/java/com/androidplay/services/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.androidplay.services.utils
 
 object Constants {
 
-    const val WEATHER_URL = "https://api.openweathermap.org/data/2.5/"
+    const val WEATHER_URL = "https://api.openweathermap.org/"
     const val KELVIN_CONSTANT = 273.15
     const val DEFAULT_AREA = "Delhi"
     const val DATASTORE_PREF_NAME = "weather_object"

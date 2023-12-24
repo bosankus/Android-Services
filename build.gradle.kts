2581d66daf5b08d521033e5fb520cb12c4127a24
Ankush Bose <ankush@androidplay.in>
2023-12-25 01:08:25 +0530

    Updated Kotlin to 2.0.0-Beta2 - updated ksp classpath version - enabled nonTransitiveRClass - enabled configuration-cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-Beta2") // Beta
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.20-1.0.14")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.0-Beta2-1.0.16") // Pre release
         classpath("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.4")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
